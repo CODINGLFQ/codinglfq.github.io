56f8d9341d0d3be5a549ecdb1e9e8e12d41f5456
lfq <lfqboik@gmail.com>
2021-03-18 13:14:47 +0800

    add maven moduel, Stream exercise

---
+++ JavaEveryDay/src/main/java/LimitAndSkipTest.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * @program: codinglfq.github.io
+ * @description: Stream流，取用前几个：limit、跳过前几个：skip
+ * @author: LFQ
+ * @create: 2021-03-18 11:35
+ *
+ * 已知ArrayList集合中有如下元素{陈玄风、梅超风、陆乘风、曲灵风、武眠风、冯默风、罗玉风}，使用Stream
+ * 1、取出前2个元素并在控制台打印输出。
+ * 2、取出后2个元素并在控制台打印输出。
+ */
+public class LimitAndSkipTest {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("陈玄风");
+        list.add("梅超风");
+        list.add("陆乘风");
+        list.add("曲灵风");
+        list.add("武眠风");
+        list.add("冯默风");
+        list.add("武眠风");
+        list.add("罗玉风");
+
+        list.stream().limit(2).forEach(System.out::println);
+
+
+    }
+}
